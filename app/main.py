a65bb1eb553375a6f2471541e553431fd85cb1f5
Abella-lab <kingabella830@gmail.com>
2025-11-26 07:47:01 +0300

    unneccesary files drop

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from .db import engine, Base, create_database_if_not_exists
-from .routers import posts, users, comments, votes
+from app.routers import posts, users, comments, votes
 from . import auth
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = ["https://www.google.com"]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 def create_tables():
     create_database_if_not_exists()
@@ -16,6 +26,7 @@ app.include_router(auth.router)
 app.include_router(votes.router)
 
 
+
 @app.get("/", status_code=202)
 async def home():
     return {"message": "Hello from home"}
@@ -23,3 +34,4 @@ async def home():
 @app.get("/landing", status_code=202)
 async def landing():
     return {"message": "Hello from landing page"}
+
